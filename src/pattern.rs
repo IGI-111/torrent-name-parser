a297bb05b0a5a3c673c4d127ecf453117e2ee0b8
Adam.S <Bas-Man@users.noreply.github.com>
2022-01-29 16:29:59 +0900

    remove the need for the `long` capture and capture two episodes. Still need to solve the issue of extracting both captures...

--- src/pattern.rs
+++ src/pattern.rs
@@ -91,7 +91,7 @@ lazy_static! {
     pub static ref SEASON: Pattern =
         regex!(r"(?i)s?(?P<short>\d+) ?[ex]|(season)(?:[^\d]|$)(?P<long>\d+)|s(?P<dash>\d+) - \d+");
     pub static ref EPISODE: Pattern = regex!(
-        r"(?i)e(?P<short>\d+)(?:[^\d]|$)|(episode)(?:[^\d]|$)(?P<long>\d+)|\d+x(?P<cross>\d+)|s\d+ - (?P<dash>\d+)"
+        r"(?i)(?:e|episode)[^.\d]?(?:(?P<short>\d+)(?:e(?P<last>\d+))?)|\d+x(?P<cross>\d+)|s\d+ - (?P<dash>\d+)"
     );
     pub static ref FILE_EXTENSION: Pattern =
         regex!(r"(?:\.)(?P<extension>[a-z]{2,4}(?:\d)?|m4v|3gp|h26[45])$");
